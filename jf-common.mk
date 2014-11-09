8adad51fd7595fa46a93ebc97170c282174f6b40
AntaresOne <antaresone@outlook.com>
2014-11-09 12:56:36 +0100

    jf-common: don't build msm8960 audio library
    Breaks audio but fixes compilation errors

--- jf-common.mk
+++ jf-common.mk
@@ -73,7 +73,6 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8960 \
-    audio.primary.msm8960 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler
